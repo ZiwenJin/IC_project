112f375f2104d15c14845e02a877a6c0cc938126
Ziwen <zj615@ic.ac.uk>
2016-09-04 02:57:18 +0100

    before writing L2 implementation, do not want change codes

--- js/draw_L3.js
+++ js/draw_L3.js
@@ -1,11 +1,15 @@
 
-var width33 = 165;
-var height33 = 165;
+// 33 for small plots
+var width33 = 155;
+var height33 = 155;
+// padding between small plots
 var margin33 = { top: 20, right: 30, bottom: 20, left: 30 };
+// L3 for the main svg
 var widthL3 = width33 * 4 + margin33.left * 11;
 var heightL3 = height33 * 3 + margin33.top * 8;
 
-
+// function to draw L3 image, with 2 specified SNIae
+// If only draw for one SNIa, the second parameter is uundefined
 function createLevel3Image(key1, key2) {
     if (key1 != undefined) { key1 -= 1; }
     if (key2 != undefined) { key2 -= 1; }
@@ -14,10 +18,13 @@ function createLevel3Image(key1, key2) {
         .attr("width", widthL3)
         .attr("height", heightL3);
 
+    // extent for all 3 variables based on 2 SNIa
     var M0Extent = d3.extent(M0[key1].concat(M0[key2]));
     var x1StarExtent = d3.extent(x1Star[key1].concat(x1Star[key2]));
     var cStarExtent = d3.extent(cStar[key1].concat(cStar[key2]));
 
+    // drawFirst indicates whether key2 is undefined
+    // only set second dataset when key2 is not undefined
     if (key2 == undefined) {
         var drawFirst = true;
         var dataset2 = { M0: undefined, x1Star: undefined, cStar: undefined };
@@ -26,7 +33,7 @@ function createLevel3Image(key1, key2) {
         var dataset2 = { M0: M0[key2], x1Star: x1Star[key2], cStar: cStar[key2] };
     }
 
-    // if want to adjust the coordinate, seems both SNIa data are required
+    // draw 6 small plots, 3 for both histograms and scatters, all with coordinates
     drawHistogram(drawFirst, 0, M0Extent, M0[key1], M0[key2]);
     drawHistogramCoordinates(0, "M0");
     drawScatter(drawFirst, 1, M0Extent, x1StarExtent, M0[key1], x1Star[key1], dataset2.M0, dataset2.x1Star);
@@ -40,7 +47,7 @@ function createLevel3Image(key1, key2) {
     drawHistogram(drawFirst, 10, cStarExtent, cStar[key1], cStar[key2]);
     drawHistogramCoordinates(10, "c*");
 
-    // testing: draw scatter between M0, x1*, c*
+    // draw additional correlation plots if 2 SNIa selected
     if (!drawFirst) {
         drawScatter2(3, d3.extent(M0[key1]), d3.extent(M0[key2]), M0[key1], M0[key2]);
         drawScatterCoordinates(3, "M0["+(key1+1)+"]", "M0["+(key2+1)+"]");
@@ -50,7 +57,7 @@ function createLevel3Image(key1, key2) {
         drawScatterCoordinates(11, "x1*["+(key1+1)+"]", "x1*["+(key2+1)+"]");
     }
 
-
+    // write messages to label the SN ID
     if (drawFirst) {
         d3.select("#mainSVG")
             .append("text")
@@ -70,36 +77,37 @@ function createLevel3Image(key1, key2) {
             })
             .attr("fill", "green")
     }
-
 }
 
+// function to draw histograms
 function drawHistogram(drawFirst, id, xExtent, xData1, xData2) {
+    // x axis attr
     var histX = d3.scaleLinear()
         .domain(xExtent)
         .range([0, width33]);
-    if (drawFirst) {
-        var bins1 = d3.histogram()
+
+    // first SN's bins
+    var bins1 = d3.histogram()
+        .domain(histX.domain())
+        .thresholds(histX.ticks(10))
+        (xData1);
+
+    // second SN's bins, only updated when !drawFirst
+    var bins2 = [];
+    // only draw bins2 when !drawFirst
+    if (!drawFirst) {
+        bins2 = d3.histogram()
             .domain(histX.domain())
-            .thresholds(histX.ticks(7))
-            (xData1);
-        var histY = d3.scaleLinear()
-            .domain([0, d3.max(bins1, function(d) { return d.length; })])
-            .range([height33, 0]);
-    } else {
-        var bins1 = d3.histogram()
-            .domain(histX.domain())
-            .thresholds(histX.ticks(7))
-            (xData1);
-        var bins2 = d3.histogram()
-            .domain(histX.domain())
-            .thresholds(histX.ticks(7))
+            .thresholds(histX.ticks(10))
             (xData2);
-        var histY = d3.scaleLinear()
-            .domain([0, d3.max(bins1.concat(bins2), function(d) { return d.length; })])
-            .range([height33, 0]);
     }
 
-    // id = [0, ..., 8]
+    // y axis attr depends on both bins
+    var histY = d3.scaleLinear()
+        .domain([0, d3.max(bins1.concat(bins2), function(d) { return d.length; })])
+        .range([height33, 0]);
+
+    // find position of small plot by id = [0, ..., 8], and then create it
     var xPos = id % 4 + 1;
     var yPos = Math.floor(id / 4) + 1;
     d3.select("#mainSVG")
@@ -111,48 +119,10 @@ function drawHistogram(drawFirst, id, xExtent, xData1, xData2) {
         .attr("x", xPos*(margin33.left + margin33.right) + (xPos-1)*width33)
         .attr("y", yPos*(margin33.top + margin33.bottom) + (yPos-1)*height33);
 
-    if (drawFirst) {
-        var x = d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisX" + id)
-            .attr("transform", "translate(0," + height33 + ")")
-            .call(d3.axisBottom(histX));
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("id", "g1")
-            .selectAll("rect")
-            .data(bins1)
-            .enter()
-            .append("rect")
-            .attr("transform", function(d) { return "translate(" + histX(d.x0) + ", " + histY(d.length) + ")"; })
-            .attr("x", 1)
-            .attr("width", function(d) { return histX(d.x1) - histX(d.x0) - 2; })
-            .attr("height", function(d) { return height33 - histY(d.length); })
-            .attr("fill", "orange");
-    } else {
+    // remove previous x axis and draw second SN when !drawFirst
+    if (!drawFirst) {
         d3.select(".axisX" + id).remove();
         d3.select("#svg" + id).select("#g1").remove();
-
-        var x = d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisX" + id)
-            .attr("transform", "translate(0," + height33 + ")")
-            .call(d3.axisBottom(histX));
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("id", "g1")
-            .selectAll("rect")
-            .data(bins1)
-            .enter()
-            .append("rect")
-            .attr("transform", function(d) { return "translate(" + histX(d.x0) + ", " + histY(d.length) + ")"; })
-            .attr("x", 1)
-            .attr("width", function(d) { return histX(d.x1) - histX(d.x0) - 2; })
-            .attr("height", function(d) { return height33 - histY(d.length); })
-            .attr("fill", "orange")
-            .attr("opacity", "0.7");
 
         d3.select("#svg" + id)
             .append("g")
@@ -166,10 +136,33 @@ function drawHistogram(drawFirst, id, xExtent, xData1, xData2) {
             .attr("width", function(d) { return histX(d.x1) - histX(d.x0) - 2; })
             .attr("height", function(d) { return height33 - histY(d.length); })
             .attr("fill", "green")
-            .attr("opacity", "0.5");
+            .attr("opacity", "0.7");
     }
+
+    // anyway, draw first SN
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("id", "g1")
+        .selectAll("rect")
+        .data(bins1)
+        .enter()
+        .append("rect")
+        .attr("transform", function(d) { return "translate(" + histX(d.x0) + ", " + histY(d.length) + ")"; })
+        .attr("x", 1)
+        .attr("width", function(d) { return histX(d.x1) - histX(d.x0) - 2; })
+        .attr("height", function(d) { return height33 - histY(d.length); })
+        .attr("fill", "orange")
+        .attr("opacity", "0.7");
+
+    // draw x axis
+    var x = d3.select("#svg" + id)
+        .append("g")
+        .attr("class", "axisX" + id)
+        .attr("transform", "translate(0," + height33 + ")")
+        .call(d3.axisBottom(histX));
 }
 
+// function to draw scatters
 function drawScatter(drawFirst, id, xExtent, yExtent, xData1, yData1, xData2, yData2) {
     var x = d3.scaleLinear()
         .domain(xExtent)
@@ -191,50 +184,10 @@ function drawScatter(drawFirst, id, xExtent, yExtent, xData1, yData1, xData2, yD
         .attr("x", xPos*(margin33.left + margin33.right) + (xPos-1)*width33)
         .attr("y", yPos*(margin33.top + margin33.bottom) + (yPos-1)*height33);
 
-    if (drawFirst) {
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisX" + id)
-            .attr("transform", "translate(" + 0 + ", " + height33 + ")")
-            .call(d3.axisBottom(x).ticks(5));
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisY" + id)
-            .attr("transform", "translate(" + 0 + ", 0)")
-            .call(d3.axisRight(y).ticks(5));
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("id", "g1")
-            .selectAll("circle")
-            .data(d3.range(xData1.length))  // .data(d3.range(xData.length))
-            .enter()
-            .append("circle")
-            .attr("cx", function(d) { return x(xData1[d]); })
-            .attr("cy", function(d) { return y(yData1[d]); })
-            .attr("r", 1)
-            .attr("fill", "orange");
-    } else {
+    if (!drawFirst) {
         d3.select(".axisX" + id).remove();
         d3.select(".axisY" + id).remove();
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisX" + id)
-            .attr("transform", "translate(" + 0 + ", " + height33 + ")")
-            .call(d3.axisBottom(x).ticks(5));
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisY" + id)
-            .attr("transform", "translate(" + 0 + ", 0)")
-            .call(d3.axisRight(y).ticks(5));
-
-        d3.select("#svg" + id).select("#g1").selectAll("circle")
-            .attr("cx", function(d) { return x(xData1[d]); })
-            .attr("cy", function(d) { return y(yData1[d]); })
-            .attr("opacity", "0.8");
+        d3.select("#svg" + id).select("#g1").remove();
 
         d3.select("#svg" + id)
             .append("g")
@@ -247,10 +200,36 @@ function drawScatter(drawFirst, id, xExtent, yExtent, xData1, yData1, xData2, yD
             .attr("cy", function(d) { return y(yData2[d]); })
             .attr("r", 1)
             .attr("fill", "green")
-            .attr("opacity", "0.8");
+            .attr("opacity", "0.3");
     }
+
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("id", "g1")
+        .selectAll("circle")
+        .data(d3.range(xData1.length))  // .data(d3.range(xData.length))
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(xData1[d]); })
+        .attr("cy", function(d) { return y(yData1[d]); })
+        .attr("r", 1)
+        .attr("fill", "orange")
+        .attr("opacity", "0.3");
+
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("class", "axisX" + id)
+        .attr("transform", "translate(" + 0 + ", " + height33 + ")")
+        .call(d3.axisBottom(x).ticks(5));
+
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("class", "axisY" + id)
+        .attr("transform", "translate(" + 0 + ", 0)")
+        .call(d3.axisRight(y).ticks(5));
 }
 
+// function to additional correlation scatter
 function drawScatter2(id, xExtent, yExtent, xData, yData) {
     var x = d3.scaleLinear()
         .domain(xExtent)
@@ -259,7 +238,7 @@ function drawScatter2(id, xExtent, yExtent, xData, yData) {
         .domain(yExtent)
         .range([height33, 0]);
 
-    // id = [0, ..., 8]
+    // actually, only id = 3, 7, 11 used
     var xPos = id % 4 + 1;
     var yPos = Math.floor(id / 4) + 1;
 
@@ -272,32 +251,32 @@ function drawScatter2(id, xExtent, yExtent, xData, yData) {
         .attr("x", xPos*(margin33.left + margin33.right) + (xPos-1)*width33)
         .attr("y", yPos*(margin33.top + margin33.bottom) + (yPos-1)*height33);
 
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("class", "axisX" + id)
+        .attr("transform", "translate(" + 0 + ", " + height33 + ")")
+        .call(d3.axisBottom(x).ticks(5));
 
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisX" + id)
-            .attr("transform", "translate(" + 0 + ", " + height33 + ")")
-            .call(d3.axisBottom(x).ticks(5));
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("class", "axisY" + id)
+        .attr("transform", "translate(" + 0 + ", 0)")
+        .call(d3.axisRight(y).ticks(5));
 
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("class", "axisY" + id)
-            .attr("transform", "translate(" + 0 + ", 0)")
-            .call(d3.axisRight(y).ticks(5));
-
-        d3.select("#svg" + id)
-            .append("g")
-            .attr("id", "g1")
-            .selectAll("circle")
-            .data(d3.range(xData.length))  // .data(d3.range(xData.length))
-            .enter()
-            .append("circle")
-            .attr("cx", function(d) { return x(xData[d]); })
-            .attr("cy", function(d) { return y(yData[d]); })
-            .attr("r", 1)
-            .attr("fill", "blue");
+    d3.select("#svg" + id)
+        .append("g")
+        .attr("id", "g1")
+        .selectAll("circle")
+        .data(d3.range(xData.length))  // .data(d3.range(xData.length))
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return x(xData[d]); })
+        .attr("cy", function(d) { return y(yData[d]); })
+        .attr("r", 1)
+        .attr("fill", "blue");
 }
 
+// function to label histogram coordinate
 function drawHistogramCoordinates(id, xLabel) {
     d3.select("#svg" + id)
         .append("text")
@@ -307,6 +286,7 @@ function drawHistogramCoordinates(id, xLabel) {
         .text(xLabel);
 }
 
+// function to label scatter coordinate
 function drawScatterCoordinates(id, xLabel, yLabel) {
     d3.select("#svg" + id)
         .append("text")
@@ -319,48 +299,3 @@ function drawScatterCoordinates(id, xLabel, yLabel) {
         .attr("y", 12)
         .text(yLabel);
 }
-
-function zoomHistogram(data, gid) {
-    return d3.zoom()
-        .scaleExtent([0.5, 5])
-        .on("zoom", function() {
-            bins = d3.histogram()
-                .domain(histX.domain())
-                .thresholds(histX.ticks(d3.event.transform.k))
-                (data);
-
-            d3.select("#g" + gid)
-                .selectAll("rect")
-                .data(bins)
-                .enter()
-                .append("rect")
-                .attr("transform", function(d) { return "translate(" + histX(d.x0) + ", " + histY(d.length) + ")"; })
-                .attr("x", 1)
-                .attr("width", histX(bins[0].x1) - histX(bins[0].x0) - 2)
-                .attr("height", function(d) { return height33 - histY(d.length); });
-
-            d3.select(".axisX" + gid).remove();
-
-            var x = d3.select("#svg" + gid)
-                .append("g")
-                .attr("class", "axisX" + gid)
-                .attr("transform", "translate(0," + height33 + ")")
-                .call(d3.axisBottom(histX));
-        });
-}
-
-function zoomScatter(gid) {
-    return d3.zoom()
-        .scaleExtent([0.5, 10])
-        //.translateExtent([[-1000, -1000], [1000, 1000]])
-        .on("zoom", function() {
-            d3.select("#" + gid).attr("transform", d3.event.transform);
-            d3.select("#" + gid)
-                .selectAll("circle")
-                .attr("r", 2 / d3.event.transform.k);
-
-        });
-}
-
-d3.select("#svg1").call(zm = zoomScatter("g1"));
-d3.select("#svg2").call(zm = zoomScatter("g2"));
